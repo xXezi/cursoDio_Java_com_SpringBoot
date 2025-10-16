50ffa66e6e7ea5b1abf051b0908d16e72793c379
William Roberti Moreira <willroberti04@hotmail.com>
2025-10-15 21:00:18 -0300

    update class main

--- 02_POO_com_java/desafioAgendaContatosMap/src/application/ExecutarAgenda.java
+++ 02_POO_com_java/desafioAgendaContatosMap/src/application/ExecutarAgenda.java
@@ -6,12 +6,12 @@ public class ExecutarAgenda {
 	public static void main(String[] args) {
 			
 		AgendaContatos agendaContatos = new AgendaContatos();
-	    agendaContatos.adicionarContato("Camila", 123456);
+	    agendaContatos.adicionarContato("Xezi", 123456);
 	    agendaContatos.adicionarContato("João", 5665);
 	    agendaContatos.adicionarContato("Carlos", 1111111);
 	    agendaContatos.adicionarContato("Ana", 654987);
 	    agendaContatos.adicionarContato("Maria", 1111111);
-	    agendaContatos.adicionarContato("Camila", 44444);
+	    agendaContatos.adicionarContato("Xezi", 44444);
 	    agendaContatos.exibirContatos();
 	    agendaContatos.removerContato("Maria");
 	    agendaContatos.exibirContatos();
