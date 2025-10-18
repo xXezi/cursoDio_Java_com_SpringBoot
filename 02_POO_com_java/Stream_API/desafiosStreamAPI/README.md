cb42d47c93e32de9cd1f630284a160856f9a1233
William Roberti Moreira <willroberti04@hotmail.com>
2025-10-18 20:18:20 -0300

    desafio 1

---
+++ 02_POO_com_java/Stream_API/desafiosStreamAPI/README.md
@@ -0,0 +1,1 @@
+# Readme oficial dos desafios: [Stream API Java](https://github.com/digitalinnovationone/ganhando_produtividade_com_Stream_API_Java/tree/master/src/stream_api).
---
+++ 02_POO_com_java/Stream_API/desafiosStreamAPI/src/application/Desafio_1.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio_1 {
+
+	public static void main(String[] args) {		
+		/*
+		 * Desafio 1 - Mostre a lista na ordem numérica:
+		 * 
+		 * Crie um programa que utilize a Stream API para ordenar a 
+		 * lista de números em ordem crescente e a exiba no console.
+		 */		
+		
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);		
+		
+		// nova lista com os números em ordem numérica
+		List<Integer> numerosOrdenados = numeros.stream()
+				.sorted()
+				.toList();
+		
+		// imprimindo valores
+		System.out.println(numerosOrdenados);
+	}
+}
