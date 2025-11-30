1bcb499d37b578e42da2e830b717c9c82721f261
xXezi <willroberti04@hotmail.com>
2025-11-30 15:12:48 -0300

    class optional

--- 02_POO_com_java/Stream_API/classOptional/src/application/ClassOptional.java
+++ 02_POO_com_java/Stream_API/classOptional/src/application/ClassOptional.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.util.Optional;
+
 public class ClassOptional {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("only test");
+	
+		Optional<String> optionalValue = Optional.ofNullable(null);
+		String result = optionalValue.orElse("Default");
+		System.out.println(result);
 	}
-
 }
