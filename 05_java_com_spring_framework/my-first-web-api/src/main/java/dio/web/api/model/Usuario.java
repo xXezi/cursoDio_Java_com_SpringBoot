5a605183148010206eaf1bf7622431582d6f8a29
xXezi <willroberti04@hotmail.com>
2025-12-06 17:56:38 -0300

    CRUD com spring boot e swagger

---
+++ 05_java_com_spring_framework/my-first-web-api/src/main/java/dio/web/api/model/Usuario.java
@@ -0,0 +1,45 @@
+package dio.web.api.model;
+
+public class Usuario {
+	private String id;
+	private String login;
+	private String password;
+	
+	public Usuario() {}
+	
+	public Usuario(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}	
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User {"+
+				"login = "+login +
+				", password = "+password;
+	}
+}
