e41dd40fc7f91926a5c80bf1e4897511b90a8ee2
xXezi <willroberti04@hotmail.com>
2025-12-11 22:20:19 -0300

    segurança com spring security

---
+++ 05_java_com_spring_framework/xezi-spring-security/src/main/java/xezi/spring/security/XeziSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package xezi.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XeziSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XeziSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ 05_java_com_spring_framework/xezi-spring-security/src/test/java/xezi/spring/security/XeziSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package xezi.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XeziSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
