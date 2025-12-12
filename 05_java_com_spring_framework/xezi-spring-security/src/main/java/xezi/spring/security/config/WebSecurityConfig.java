96822cc29625ad986863c37bb69ed0c81e42e8e6
xXezi <willroberti04@hotmail.com>
2025-12-11 23:39:47 -0300

    autenticação com banco de dados

--- 05_java_com_spring_framework/xezi-spring-security/src/main/java/xezi/spring/security/config/WebSecurityConfig.java
+++ 05_java_com_spring_framework/xezi-spring-security/src/main/java/xezi/spring/security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
-package xezi.spring.security;
+package xezi.spring.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,12 +8,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-
+	
+	@Autowired
+	private SecurityDatabaseService securityService;
+	
+	@Autowired
+	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
@@ -20,9 +30,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(HttpMethod.POST, "/login").permitAll()
 			.antMatchers("/managers").hasAnyRole("MANAGERS")
 			.antMatchers("/users").hasAnyRole("USERS","MANAGERS")
-			.anyRequest().authenticated().and().formLogin();
+			.anyRequest().authenticated().and().httpBasic();
 	}
 	
+	/*
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.inMemoryAuthentication()
@@ -33,5 +44,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.withUser("admin")
 			.password("{noop}master123")
 			.roles("MANAGERS");
-	}	
+	}	*/
 }
--- 05_java_com_spring_framework/xezi-spring-security/src/main/java/xezi/spring/security/controller/WelcomeController.java
+++ 05_java_com_spring_framework/xezi-spring-security/src/main/java/xezi/spring/security/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package xezi.spring.security;
+package xezi.spring.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
