69021791586ad8a260173f50c964ca423da464a6
xXezi <willroberti04@hotmail.com>
2025-12-14 16:21:07 -0300

    mapeando entidades e criado o perfil de dev

---
+++ 05_java_com_spring_framework/spring-api-nuvem/src/main/java/xezi/spring/SpringApiNuvemApplication.java
@@ -0,0 +1,13 @@
+package xezi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiNuvemApplication.class, args);
+	}
+
+}
---
+++ 05_java_com_spring_framework/spring-api-nuvem/src/main/java/xezi/spring/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package xezi.spring.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import xezi.spring.domain.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+}
